f339fcf8dd49b3d13bcaa7214f63789e8bad44aa
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-06-15 09:37:57 -0500

    first commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function userProfile(){
+
+    }
+
+    public function logout(){
+
+    }
+}
