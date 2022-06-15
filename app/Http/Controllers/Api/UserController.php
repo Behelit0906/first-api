79e18aa755c503dde9b1e1eea44bb66c954ddb3e
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-06-15 10:21:35 -0500

    register method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,19 +9,39 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|min:3|max:15',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|max:15|confirmed',
+        ]);
+
+
+        $data = $request->all();
+        $data['password'] = Hash::make($data['password']);
+
+        User::create($data);
+
+        return response()->json([
+            'status' => 1,
+            'msg' => 'successful registration',
+        ]);
 
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
 
     }
 
-    public function userProfile(){
+    public function userProfile()
+    {
 
     }
 
-    public function logout(){
+    public function logout()
+    {
 
     }
 }
