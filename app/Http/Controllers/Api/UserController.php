ba1349ab11592a0734b67f77cabf6cee788bde53
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-06-15 11:45:24 -0500

    login, profile and logout method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+
+
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        $validated = $request->validate([
+        $request->validate([
             'name' => 'required|min:3|max:15',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8|max:15|confirmed',
@@ -32,16 +35,53 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8|max:15',
+        ]);
+
+        if(Auth::attempt($request->all())){
+
+            $user = User::where('email',$request->email)->first();
+            $user->tokens()->delete();
+            $token = $user->createToken('token');
+
+            $data = [
+                'status' => 1,
+                'msg' => 'successful login',
+                'token' =>$token->plainTextToken,
+            ];
+        
+        }
+        else{
+            $data = [
+                'status' => 0, 
+                'msg' => 'Incorrect credentials',
+            ];    
+        }
+
+        return response()->json($data);
 
     }
 
     public function userProfile()
     {
-
+        $user = Auth::user();
+        return response()->json([
+            'status' => 1,
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
     }
 
     public function logout()
     {
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
 
+        return response()->json([
+            'status' => 1,
+            'msg' => 'Bye, see you later :)',
+        ]);
     }
 }
